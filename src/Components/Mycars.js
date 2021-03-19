c5fb9558b0dcc5d2feef488ea74812126eb3a051
BELLARABI Ibtihal <bellarabiibtihal@gmail.com>
2021-03-19 16:46:44 +0100

    Second version

--- src/Components/Mycars.js
+++ src/Components/Mycars.js
@@ -5,15 +5,24 @@ class Mycars extends React.Component{
     noCopy=()=>{
         alert ('Copy Denied');
     }
+    state={
+        voitures:[{name:'ford',color:'red',year:2000},{name:'nissan',color:'yellow',year:2010},{name:'peugeot',color:'green',year:2001}],
+        titre:'Mon catalogue 2'
+    }
+    AddNumber=()=>{
+        const updatedState=this.state.voitures.map((param)=>{return param.year-=10});
+        this.setState({updatedState})
+    }
     render()
-    {
+    {   const year =new Date().getFullYear();
         return(
-          <div>
-           <h1>{this.props.title}</h1>
+           <div>
+           <h1>{this.state.titre}</h1>
            <p onCopy={this.noCopy}>Salut, voici notre catalogue </p>
-           <Car couleur='red'>Ford</Car>
-           <Car couleur='yellow'>Mercedes</Car>
-           <Car >Peugeot</Car>
+           <button onClick={this.AddNumber}>-10ans</button>
+           {this.state.voitures.map((voiture,index)=>{
+               return(<Car key={voiture.index} couleur={voiture.color} annee={year-voiture.year+' ans'} >{voiture.name}</Car>);
+           })}
            </div> 
         );
     }
